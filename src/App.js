c967ac787a6f039c3b451e6b139f4247db24f870
Md Faisal Raza Khan <irkfaisal@gmail.com>
2022-01-10 23:54:23 +0530

    second commit ui-part

--- src/App.js
+++ src/App.js
@@ -1,12 +1,55 @@
+import React from 'react';
+import {Container, Card, CardContent, makeStyles, Grid, TextField, Button} from '@material-ui/core';
 
-import './App.css';
 
-function App() {
+function App() { 
+  const classes = useStyles();
+
   return (
-    <div className="App">
-        <h1>hello world</h1>
-    </div>
+    <Container className={classes.conatiner}>
+          <Card>
+              <h2 className={classes.title}>Generate Download & Scan QR Code with React js</h2>
+              <CardContent>
+                  <Grid container spacing={2}>
+                      <Grid item xl={4} lg={4} md={6} sm={12} xs={12}>
+                          <TextField label="Enter Text Here" />
+                          <Button className={classes.btn} variant="contained" 
+                            color="primary">Generate</Button>
+                            {/* <br/>
+                            <br/>
+                            <br/> */}
+                      </Grid>
+                      <Grid item xl={4} lg={4} md={6} sm={12} xs={12}>
+                        <Button className={classes.btn} variant="contained" color="secondary" >Scan Qr Code</Button>
+                        <h3>Scanned Code: </h3>
+                      </Grid>
+                      <Grid item xl={4} lg={4} md={6} sm={12} xs={12}>
+                         <h3>Qr Code Scan by Web Cam</h3>
+                         <h3>Scanned By WebCam Code:</h3>
+                      </Grid>
+                  </Grid>
+              </CardContent>
+          </Card>
+    </Container>
   );
 }
 
+const useStyles = makeStyles((theme) => ({
+    conatiner: {
+      marginTop: 10
+    },
+    title: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems:  'center',
+      background: '#3f51b5',
+      color: '#fff',
+      padding: 20
+    },
+    btn : {
+      marginTop: 10,
+      marginBottom: 20
+    }
+}));
 export default App;
+
